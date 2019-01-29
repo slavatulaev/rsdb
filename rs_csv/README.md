a0a4e164622eff1ff19bb671d91e80341c1f46cc
Konstantin Kruts <kkruts@yahoo.com>
2019-01-29 23:39:04 +0800

    smth new

---
+++ rs_csv/README.md
@@ -0,0 +1,3 @@
+# rsdb
+in this directory should be placed .csv files with RS results 
+from here collected by RS data is imported to database
--- rsdb.py
+++ rsdb.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 ########## python inteface to RSDB
 
+import csv
+import os
 import sys
+
 import mysql.connector
 from netaddr import *
 import pprint
@@ -12,6 +15,8 @@ dbServerAddress = sys.argv[1]
 dbDBName = sys.argv[2]
 dbUserName = sys.argv[3]
 dbPassword = sys.argv[4]
+#dbPassword = '9CMeAhDpu8[#QtI!qaZ'
+print(dbServerAddress, dbDBName, dbUserName, dbPassword)
 
 ########################
 def printALine():       # prints a divider 
@@ -27,8 +32,8 @@ def dbTest():           # not used in code - delete afterwards
     cursor = dbRS.cursor()
     query = ("SELECT * FROM IP_DIAPAZONS")
     cursor.execute(query)
-    for (IPDIA, SCANNED, COUNTRY, STATE, CITY, PROVIDER, DATE) in cursor:
-     print("{}, {}, {}, {}, {}, {}, {}".format(IPDIA, SCANNED, COUNTRY, STATE, CITY, PROVIDER, DATE))
+    for (IPDIA, COUNTRY, STATE, CITY, PROVIDER, DATE) in cursor:
+     print("{}, {}, {}, {}, {}, {}".format(IPDIA, COUNTRY, STATE, CITY, PROVIDER, DATE))
     cursor.close()
     dbRS.close()
     return
@@ -39,8 +44,8 @@ def mainMenu():
     printALine()
     print('0 : Exit script')
     print('1 : Check and add IP Ranges')
-    print('2 : Add New Data from RouterScan (not working yet)') 
-    print('3 : Working with routers info (not working yet)')
+    print('2 : Add New Data from RouterScan') 
+    print('3 : Working with routers info (not ready yet)')
     printALine()
     return input()
 
@@ -54,13 +59,23 @@ def subMenu1():         # menu for work with ip ranges
     printALine()
     return input()
 
+def subMenu2():         # menu for Add New Data from RouterScan
+    printALine()
+    print('=============== Add New Data from RouterScan Submenu =============')
+    printALine()
+    print('0 : Go to Main Menu')
+    print('1 : Add New Data from RouterScan')
+    print('2 : ... < not ready yet >')
+    printALine()
+    return input()
+
 def subSubMenu1():      # IP Range Tools subsubmenu
     printALine()
     print('=============== IP Tools Submenu =============')
     printALine()
     print('0 : Go to IP Ranges Submenu')
-    print('1 : Check and optimize IP Ranges in DB')
-    print('2 : Devide big network into parts')
+    print('1 : Check and optimize IP Ranges in DB (not ready yet)')
+    print('2 : Devide big network into parts (not ready yet)')
     return input()
 
 def checkIPRange():     # checking ip ranges by list and adding new to database
@@ -130,10 +145,65 @@ def checkIPRange():     # checking ip ranges by list and adding new to database
             print('Ok, you choose not to add this data to online Database. \n Exiting to menu.')
             break
         else:
-            print("Wrong input. Choose 'y' or 'y'")
+            print("Wrong input. Choose 'y' or 'n'")
     cursor.close()
     dbRS.close()
     return        
+
+def addNewDataFromRS():
+    insertQuery = ("INSERT INTO SCANRESS "
+                "(IP, PORT, LOGPASS, DEVICE, BSSID, ESSID, SECURITY, WKEY, WPSPIN, LANIP, LANMASK, WANIP, WANMASK, WANGATEWAY, DNS, COMMENTS) "
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+
+    selectQuery = "SELECT * FROM SCANRESS WHERE IP = %s AND PORT = %s"
+
+    updateQuery = "UPDATE SCANRESS SET LOGPASS = %s, DEVICE = %s, BSSID = %s, ESSID = %s, SECURITY = %s, WKEY = %s, WPSPIN = %s, LANIP = %s, LANMASK = %s, WANIP = %s, WANMASK = %s, WANGATEWAY = %s, DNS = %s, COMMENTS = %s WHERE IP = %s AND PORT = %s"
+
+    insCounter, updCounter = 0, 0
+
+    print('Connecting to online DataBase.... Please Wait....')
+    dbRS = mysql.connector.connect(
+        user = dbUserName,
+        password = dbPassword,
+        host = dbServerAddress,
+        database = dbDBName)
+    cursor = dbRS.cursor()
+    print("Input the path to directory with .csv files (for default location 'rs_csv/' in current directory - just press Enter)")
+    dirPath = input()
+    if  dirPath == '': dirPath = 'rs_csv/'
+    filesList = filter(lambda x: x.endswith('.csv'), os.listdir(dirPath)) 
+    for fileName in filesList:
+        with open(dirPath+fileName) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=';')
+            for row in readCSV:
+                if (row[0] == 'IP Address') or (row[4] == ''): continue
+                selectArgs = (row[0], row[1])
+                print(selectQuery % selectArgs)
+                cursor.execute(selectQuery, selectArgs)
+                result = cursor.fetchall()
+                if len(result) == 0:
+                    insertArgs = (row[0], row[1], row[4], row[5], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[21])
+                    print(insertQuery % insertArgs)
+                    cursor.execute(insertQuery, insertArgs)
+                    dbRS.commit()
+                    insCounter = insCounter + 1
+                else:
+                    print('such IP:PORT already in DB')
+                    if (row[4] != result[0][2]) or (row[5] != result[0][3]) or (row[8] != result[0][4]) or (row[9] != result[0][5]) or (row[10] != result[0][6]) or (row[11] != result[0][7]) or (row[12] != result[0][8]) or (row[13] != result[0][9]) or (row[14] != result[0][10]) or (row[15] != result[0][11]) or (row[16] != result[0][12]) or (row[17] != result[0][13]) or (row[18] != result[0][14]) or (row[21] != result[0][15]):
+                        updateArgs = (row[4], row[5], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[21], row[0], row[1])
+                        print(updateQuery % updateArgs)
+                        cursor.execute(updateQuery, updateArgs)
+                        dbRS.commit()
+                        updCounter = updCounter + 1
+    cursor.close()
+    dbRS.close()
+
+    print('')
+    print('=================================================================================')
+    print('Totally inserted %i rows and updated %i rows' % (insCounter, updCounter))
+    print('')
+    print('')
+    return
 
 def optimizeIPRangesDB(): 
     ipNetsInDB = IPSet() 
@@ -150,6 +220,8 @@ def optimizeIPRangesDB():
     for IPDIA in cursor:
         ipNetsInDB.add(IPDIA[0])
 
+    cursor.close()
+    dbRS.close()
     return
 
 def subSubMenu1execution():
@@ -174,6 +246,17 @@ def subMenu1execution():
             print('Here will be some more functions')
     return 
 
+def subMenu2execution():
+    while True:
+        sm1res = subMenu1()
+        if sm1res == '0':
+            break
+        elif sm1res == '1':
+            addNewDataFromRS()
+        elif sm1res == '2':
+            print('Here will be some more functions')
+    return 
+
 while True:
     mmres = mainMenu()
     if mmres == '0':
@@ -181,7 +264,7 @@ while True:
     elif mmres == '1':
         subMenu1execution()
     elif mmres == '2':
-        print('Add New Data from RouterScan (not working yet)')
+        subMenu2execution()
     elif mmres == '3':
         print('Working with routers info (not working yet)')
     else:
